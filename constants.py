1b3869b0ea45c50a8fd13cd6b6e06e8720398764
Maverick Wong Ke Jun <maverick@Mavericks-MacBook-Air-2.local>
2026-03-19 21:02:09 +0800

    Added script to rank cryptos

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import os
 
 MODEL_TYPES = ["rf", "xgb"]
 
-SYMBOLS = ["BTCUSDT", "ETHUSDT", "ADAUSDT", "BNBUSDT", "XRPUSDT"]
+SYMBOLS = ["BTCUSDT", "ETHUSDT", "ADAUSDT", "BNBUSDT", "XRPUSDT", "SOLUSDT", "LINKUSDT", "DOTUSDT", "LTCUSDT", "AVAXUSDT"]
 MARKET = "spot"          # "spot" or "futures/um"
 INTERVAL = "1m"
 MONTHS = [
