dd0c3d48395431e4ae583ddb0738aacbd2acfc0a
Maverick Wong Ke Jun <maverick@Mavericks-MacBook-Air-2.local>
2026-03-19 19:34:30 +0800

    added early termination for model training to reduce training time

--- models.py
+++ models.py
@@ -35,6 +35,10 @@ def _rf_objective(trial: optuna.Trial, X_train, y_train, X_valid, y_valid) -> fl
     trial.set_user_attr("ic", ic)
     trial.set_user_attr("rank_ic", ric)
 
+    trial.report(ric, step=0)
+    if trial.should_prune():
+        raise optuna.TrialPruned()
+
     return ric
 
 def _xgb_objective(trial: optuna.Trial, X_train, y_train, X_valid, y_valid) -> float:
